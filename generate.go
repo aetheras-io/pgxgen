bc449574d51410612371382d754329f35b6f5b98
Denis Tsai <denistsai@aetheras.io>
2019-09-09 16:36:51 +0800

    fixed genfile name bug

--- generate.go
+++ generate.go
@@ -149,7 +149,7 @@ func generateCmd(cmd *cobra.Command, args []string) {
 	}
 
 	for _, t := range c.Tables {
-		file, err := os.Create(goCaseToFileCase(t.StructName) "_gen.go")
+		file, err := os.Create(goCaseToFileCase(t.StructName) + "_gen.go")
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
